9150c4a66bd30e7268fc5e3156c80a1584f0b8c1
tamara-danilova2105 <sap1028@mail.ru>
2023-09-21 19:27:45 +0400

    upd

--- src/App.js
+++ src/App.js
@@ -9,23 +9,23 @@ import { useSelector } from "react-redux";
 import { getMyTravel } from './redux/myTravelSlice';
 import HomePage from './pages/HomePage';
 import MyTravel from './pages/MyTravel';
-import fone from './photo/fone.jpg'
+import fone from './photo/fone.jpg';
 
 function App() {
-  const items = useSelector(getMyTravel)
+  const items = useSelector(getMyTravel);
 
   return (
     <Router>
-    <img className='fone' src={fone} alt='фоновое изображение' />
-    <div className='navbar'>
-      <Link to='/' className='link'>Путешествуй по России!</Link>
-      <Link to='/mytravel' className='link'> <span className='quantity-region'>{items.length}/85</span> Мои путешествия</Link>
-    </div>
+      <img className='fone' src={fone} alt='фоновое изображение' />
+      <div className='navbar'>
+        <Link to='/' className='link'>Путешествуй по России!</Link>
+        <Link to='/mytravel' className='link'> <span className='quantity-region'>{items.length}/85</span> Мои путешествия</Link>
+      </div>
 
-    <Routes>
-      <Route path='/' element={ <HomePage/> }></Route>
-      <Route path='/mytravel' element={ <MyTravel/> }></Route>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/mytravel' element={<MyTravel />}></Route>
+      </Routes>
     </Router>
   );
 }
